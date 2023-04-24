05083dbf49c932d11b252443f7d46058c54504d1
miguel-neiva <a92945@alunos.uminho.pt>
2023-04-25 00:45:46 +0200

    HEXA,BINARY,OCTAL, amanhã corrijo o number para floats e expoentes

--- analisador_lexico.py
+++ analisador_lexico.py
@@ -1,13 +1,13 @@
 import ply.lex as lex
 
-tokens = ('COMMENT','COMMA','DOT','VAR',
-          'LEFTBRACKET','RIGHTBRACKET','LEFTSQUAREBRACKET','RIGHTSQUAREBRACKET',
-          'EQUAL','NUMBER','STRING','MSTRING', 'BOOLEAN',
-          'DATE','TIME','DATETIME')
+tokens = ('COMMENT', 'COMMA', 'DOT', 'VAR',
+          'LEFTBRACKET', 'RIGHTBRACKET', 'LEFTSQUAREBRACKET', 'RIGHTSQUAREBRACKET',
+          'EQUAL', 'NUMBER', 'STRING', 'MSTRING', 'BOOLEAN',
+          'DATE', 'TIME', 'DATETIME', 'HEXADECIMAL','BINARY','OCTAL')
 
 t_COMMENT = r'\#.*'
 t_COMMA = r'\,'
-t_DOT = r'\.' 
+t_DOT = r'\.'
 t_LEFTSQUAREBRACKET = r'\['
 t_RIGHTSQUAREBRACKET = r'\]'
 t_LEFTBRACKET = r'\{'
@@ -16,32 +16,37 @@ t_EQUAL = r'\='
 t_VAR = r'(\w+|\-)+'
 t_BOOLEAN = r'(false|true)'
 t_STRING = r'(\"[^"]+\")'
-t_MSTRING = r'\"\"\"(.*|\n)+\"\"\"' 
+t_MSTRING = r'\"\"\"(.*|\n)+\"\"\"'
 t_DATE = r'(\d{4}\-\d{2}\-\d{2})'
 t_TIME = r'(\d{2}:\d{2}:\d{2}(\.\d{6})?)'
 t_DATETIME = r'(\d{4}\-\d{2}\-\d{2}T\d{2}\:\d{2}\:\d{2}(\.\d{6})?)'
-t_NUMBER = r'(\+|\-)?(\d+(\.\d+)*((e|E)+(\+|\-)?\d+)*|inf|nan)+' # necessário corrigir o number para floats e expoentes
-
+t_NUMBER = r'(\+|\-)?(\d+(\.\d+)*((e|E)+(\+|\-)?\d+)*|inf|nan)+'  # necessário corrigir o number para floats e expoentes
+t_HEXADECIMAL = r'0x[0-9A-Fa-f]([0-9A-Fa-f]|_[0-9A-Fa-f])*'
+t_BINARY = r'0b[01]([01]|_[01])*'
+t_OCTAL = '0o[0-7]([0-7]|_[0-9])*'
 
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-t_ignore  = ' \t'
+
+t_ignore = ' \t'
+
 
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
+
 lexer = lex.lex()
 
-f = open('./TOML/toml4.toml','r')
+f = open('./TOML/toml4.toml', 'r')
 lines = f.readlines()
 
-for line in lines:    
+for line in lines:
     lexer.input(line)
     while True:
         tok = lexer.token()
-        if not tok: 
-            break      # No more input
+        if not tok:
+            break  # No more input
         print(tok)
