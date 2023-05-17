bced31b38cf5119a408cda99f652497da339a696
ederzito88 <tiagoluis.sporting@sapo.pt>
2023-05-17 23:40:05 +0100

    Todos os tipos de datas a ser bem reconhecidos

--- src/analisador_lexico.py
+++ src/analisador_lexico.py
@@ -2,9 +2,10 @@ import ply.lex as lex
 
 tokens = ('COMMENT', 'COMMA', 'DOT', 'VAR',
           'LEFTBRACKET', 'RIGHTBRACKET', 'LEFTSQUAREBRACKET', 'RIGHTSQUAREBRACKET',
-          'EQUAL', 'INT','FLOAT','INF', 'NAN', 'STRING', 'MSTRING', 'BOOLEAN',
-          'DATE', 'TIME', 'DATETIME','OFFSET','OFFSETDATETIME', 'HEXADECIMAL','BINARY','OCTAL',
+          'EQUAL', 'DATE', 'INT', 'FLOAT', 'INF', 'NAN', 'STRING', 'MSTRING', 'BOOLEAN',
+          'TIME', 'DATETIME', 'OFFSET', 'OFFSETDATETIME', 'HEXADECIMAL', 'BINARY', 'OCTAL',
           'NEWLINE')
+
 
 t_COMMA = r'\,'
 t_DOT = r'\.'
@@ -13,15 +14,9 @@ t_RIGHTSQUAREBRACKET = r'\]'
 t_LEFTBRACKET = r'\{'
 t_RIGHTBRACKET = r'\}'
 t_EQUAL = r'\='
-t_VAR = r'(\w+|\-)+'
 t_BOOLEAN = r'(false|true)'
 t_STRING = r'(\"[^"]+\")'
-t_MSTRING = r'((\"{3})[^"\\\n]+(\"{3}))'
-t_DATE = r'\d{4}-\d{2}-\d{2}'
-t_TIME = r'\d{2}:\d{2}:\d{2}(\.\d{6})?'
-t_DATETIME = fr'{t_DATE}T{t_TIME}'
-t_OFFSET = r'[+-]\d{2}:\d{2}'
-t_OFFSETDATETIME = fr'{t_DATETIME} {t_OFFSET}'
+t_MSTRING = r'\"\"\"[^\"]*\"\"\"'
 t_INT = r'[\+\-]?(0|[1-9](\_?[0-9])*)'
 t_FLOAT = r'[\+\-]?(0|[1-9](\_?[0-9])*)(\.[0-9](\_?[0-9])*([eE][\+\-]?[0-9](\_?[0-9])*)?|[eE][\+\-]?[0-9](\_?[0-9])*)'
 t_INF = r'[\+\-]?inf'
@@ -29,10 +24,31 @@ t_NAN = r'[\+\-]?nan'
 t_HEXADECIMAL = r'0x[0-9A-Fa-f]([0-9A-Fa-f]|_[0-9A-Fa-f])*'
 t_BINARY = r'0b[01]([01]|_[01])*'
 t_OCTAL = '0o[0-7]([0-7]|_[0-9])*'
+t_VAR = r'(\w+|\-)+'
 
 t_COMMENT = r'\#\s*.*'
 t_ignore = ' \t'
 
+def t_TIME(t):
+    r'\d{2}:\d{2}:\d{2}(\.\d{6})?'
+    return t
+
+
+def t_OFFSETDATETIME  (t):
+    r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{6})? [+-]\d{2}:\d{2}'
+    return t
+
+def t_DATETIME (t):
+    r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{6})?'
+    return t
+
+def t_OFFSET (t):
+    r'[+-]\d{2}:\d{2}'
+    return t
+
+def t_DATE(t):
+    r'\d{4}-\d{2}-\d{2}'
+    return t
 
 def t_NEWLINE(t):
     r'\n'
